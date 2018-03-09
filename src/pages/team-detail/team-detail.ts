a3c6a2bddc1f61e6ec9f0a4a752371333ea35689
Serhiy Konyk <konyk93@gmail.com>
2018-03-08 22:40:54 -0500

    Added game page

--- src/pages/team-detail/team-detail.ts
+++ src/pages/team-detail/team-detail.ts
@@ -1,6 +1,9 @@
 import { MyTeamsPage } from './../my-teams/my-teams';
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { EliteApi } from '../../providers/elite-api/elite-api';
+import * as _ from 'lodash';
+import { GamePage } from '../game/game';
 
 @Component({
   selector: 'page-team-detail',
@@ -9,14 +12,51 @@ import { NavController, NavParams } from 'ionic-angular';
 export class TeamDetailPage {
 
   public team: any = {};
+  public games: any[];
+  private tourneyData: any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-    this.team = this.navParams.data;
-    console.log('**nav paarams', this.navParams);
-  }
+  constructor(public navCtrl: NavController,
+    public navParams: NavParams,
+    private eliteApi: EliteApi) { }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad TeamDetailPage');
+    this.team = this.navParams.data;
+
+    this.tourneyData = this.eliteApi.getCurrentTourney();
+    this.games = _.chain(this.tourneyData.games)
+      .filter(g => g.team1Id === this.team.id || g.team2Id === this.team.id)
+      .map(g => {
+        let isTeam1 = (g.team1Id === this.team.id);
+        let opponentName = isTeam1 ? g.team2 : g.team1;
+        let scoreDisplay = this.getScoreDispay(isTeam1, g.team1Score, g.team2Score);
+        return {
+          gameId: g.id,
+          opponent: opponentName,
+          time: Date.parse(g.time),
+          location: g.location,
+          locationUrl: g.locationUrl,
+          scoreDispay: scoreDisplay,
+          homeAway: (isTeam1 ? "vs." : "at")
+        };
+      })
+      .value();
+  }
+
+  getScoreDispay(isTeam1, team1Score, team2Score) {
+    if (team1Score && team2Score) {
+      var teamScore = (isTeam1 ? team1Score : team2Score);
+      var opponentScore = (isTeam1 ? team2Score : team1Score);
+      var winIndicator = teamScore > opponentScore ? "W: " : "L: ";
+      return winIndicator + teamScore + "-" + opponentScore;
+    }
+    else {
+      return "";
+    }
+  }
+
+  gameClicked($event, game) {
+    let sourceGame = this.tourneyData.games.find(g => g.id === game.gameId);
+    this.navCtrl.parent.parent.push(GamePage, sourceGame);
   }
 
 }
