89d38b876507a0b84b356eb05ed24d5f96a33353
Serhiy Konyk <konyk93@gmail.com>
2018-03-09 21:51:26 -0500

    Added the custom favorites list

--- src/pages/my-teams/my-teams.ts
+++ src/pages/my-teams/my-teams.ts
@@ -1,18 +1,44 @@
+import { StatusBar } from '@ionic-native/status-bar';
+import { TournamentsPage } from './../tournaments/tournaments';
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular'
-import { TournamentsPage } from '../tournaments/tournaments';
+import { IonicPage, NavController, NavParams, LoadingController } from 'ionic-angular'
+import { TeamHomePage } from '../team-home/team-home';
+import { EliteApi } from '../../providers/elite-api/elite-api';
 
 @Component({
   selector: 'page-my-teams',
   templateUrl: 'my-teams.html'
 })
 export class MyTeamsPage {
-
-  constructor(private nav: NavController) {
+  favorites = [
+    {
+      team: { id: 6182, name: 'HC Elite 7th', coach: 'Michelotti' },
+      tournamentId: '3dd50aaf-6b03-4497-b074-d81703f07ee8',
+      tournamentName: 'March madness Tournament'
+    },
+    {
+      team: { id: 6183, name: 'Hooping', coach: 'Johnson' },
+      tournamentId: '3dd50aaf-6b03-4497-b074-d81703f07ee8',
+      tournamentName: 'Hoops Challange'
+    }
+  ]
+  constructor(private nav: NavController,
+    private loadingController: LoadingController,
+    private eliteApi: EliteApi) {
 
   }
 
   goToTournaments() {
     this.nav.push(TournamentsPage);
+  }
+
+  favoriteTapped($event, favorite) {
+    let loader = this.loadingController.create({
+      content: 'Loading...',
+      dismissOnPageChange: true
+    });
+    loader.present();
+    this.eliteApi.getTournamentData(favorite.tournamentId)
+      .subscribe(t => this.nav.push(TeamHomePage, favorite.team));
   }
 }
