4b7a155a93e1569a001aab67de0f90f429c00337
Serhiy Konyk <konyk93@gmail.com>
2018-03-10 21:50:21 -0500

    Added division banners

--- src/pages/teams/teams.ts
+++ src/pages/teams/teams.ts
@@ -3,6 +3,7 @@ import { TeamHomePage } from './../team-home/team-home';
 import { TeamDetailPage } from './../team-detail/team-detail';
 import { Component } from '@angular/core';
 import { NavController, NavParams, Loading, LoadingController } from 'ionic-angular';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'page-teams',
@@ -10,22 +11,35 @@ import { NavController, NavParams, Loading, LoadingController } from 'ionic-angu
 })
 export class TeamsPage {
   public teams = [];
+  private allTeams: any;
+  private allTeamDivisions: any;
 
   constructor(public navCtrl: NavController,
+    public loadingController: LoadingController,
     public navParams: NavParams,
-    private eliteApi: EliteApi,
-    public loadingController: LoadingController) {
+    private eliteApi: EliteApi) {
   }
 
   ionViewDidLoad() {
     let selectedTourney = this.navParams.data;
+
     let loader = this.loadingController.create({
       content: 'Loading...'
     });
 
     loader.present().then(() => {
       this.eliteApi.getTournamentData(selectedTourney.id).subscribe(data => {
-        this.teams = data.teams;
+        this.allTeams = data.teams;
+
+        this.allTeamDivisions =
+          _.chain(data.teams)
+            .groupBy('division')
+            .toPairs()
+            .map(item => _.zipObject(['divisionName', 'divisionTeams'], item))
+            .value();
+
+        this.teams = this.allTeamDivisions;
+        console.log('division teams', this.teams);
         loader.dismiss();
       });
     });
